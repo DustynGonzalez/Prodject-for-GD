905cff2f8df59dbd519385d497fc23bfbd516808
Lily.c <lilyconnell12@gmail.com>
2019-05-22 13:16:06 -0700

    a
    a

---
+++ oklob/Assets/Banimator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Banimator : MonoBehaviour
+{
+    private Animator _banim;
+
+    void Start()
+    {
+        _banim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKey((KeyCode.A)) || Input.GetKey((KeyCode.D)) || (Input.GetKey((KeyCode.W))) || (Input.GetKey((KeyCode.S)))){
+            _banim.SetBool("AniL", true);
+        } else {
+            _banim.SetBool("AniL", false);
+        }
+
+    }
+}
+
